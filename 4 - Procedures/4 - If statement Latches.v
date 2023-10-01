69c92d653d8aaf5b4951ab56cf17d2cac082b6fc
Harsh Choudhary <100677801+harshchoudhary1233@users.noreply.github.com>
2023-10-01 16:28:48 +0530

    Update 4 - If statement Latches.v

--- 4 - Procedures/4 - If statement Latches.v
+++ 4 - Procedures/4 - If statement Latches.v
@@ -6,13 +6,22 @@ module top_module (
     output reg keep_driving  ); //
 
     always @(*) begin
-        if (cpu_overheated)
+        if (cpu_overheated) begin
            shut_off_computer = 1;
+        end
+        else begin
+            shut_off_computer = 0;
+        end
+        
     end
 
     always @(*) begin
-        if (~arrived)
+        if (~arrived)begin
            keep_driving = ~gas_tank_empty;
+        end
+        else begin
+            keep_driving = 1'b0;
+        end
     end
 
 endmodule
